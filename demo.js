64b57fa722cc13070fa2a0465b244f41a2f7c2fe
yokselzok <yokselzok@gmail.com>
2017-01-10 23:25:13 +0300

    + demo.js

---
+++ demo.js
@@ -0,0 +1,56 @@
+var doc = document;
+var controlsPanel = doc.querySelector('.controls-panel');
+var stickyPoint = doc.querySelector('.sticky-point');
+
+searchControlsPanel();
+
+function searchControlsPanel() {
+  controlsPanel = doc.querySelector('.controls-panel');
+
+  if ( !controlsPanel ) {
+    setTimeout(searchControlsPanel, 1000);
+  }
+  else {
+    controlsPanel.addEventListener('click', takeOutPanel);
+  }
+}
+
+//---------------------------------------------
+
+function takeOutPanel () {
+  var offsetTop = controlsPanel.offsetTop - window.scrollY;
+  var offsetLeft = controlsPanel.offsetLeft - window.scrollX;
+
+  controlsPanel.style.position = 'fixed';
+  controlsPanel.style.top = offsetTop + 'px';
+  controlsPanel.style.left = offsetLeft + 'px';
+  controlsPanel.style.right = 'auto';
+  controlsPanel.style.bottom = 'auto';
+  adjustPosition();
+
+  doc.documentElement.appendChild( controlsPanel );
+
+  controlsPanel.removeEventListener('click', takeOutPanel);
+
+  stickyPoint.classList.add('sticky-point--collapse');
+}
+
+//---------------------------------------------
+
+function adjustPosition() {
+  var styles = getComputedStyle(controlsPanel);
+  var left = +styles.left.replace(/px/,'');
+  var right = +styles.right.replace(/px/,'');
+  var top = +styles.top.replace(/px/,'');
+  var bottom = +styles.bottom.replace(/px/,'');
+
+  if ( left > right ) {
+    controlsPanel.style.right = styles.right;
+    controlsPanel.style.left = 'auto';
+  }
+
+  if ( top > bottom ) {
+    controlsPanel.style.bottom = styles.bottom;
+    controlsPanel.style.top = 'auto';
+  }
+}
